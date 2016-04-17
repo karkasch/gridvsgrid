3fd15ca5266ef337e0cefb1e0031924562fa9916
karkasch <nutzer12@gmail.com>
2016-04-17 23:31:08 +0200

    Programmatically refresh grid

--- WebApplication5/wwwroot/app1/kendogrid-controller.ts
+++ WebApplication5/wwwroot/app1/kendogrid-controller.ts
@@ -8,18 +8,19 @@ module App {
         public title: string;
         public selectedItem: string;
 
-        public gridOptions: kendo.ui.GridOptions;
+        public gridOptions: kendo.ui.GridOptions; // http://docs.telerik.com/kendo-ui/AngularJS/the-grid-widget
+        public grid: kendo.ui.Grid;
 
-        constructor($scope: angular.IScope) {
+        constructor(private $scope: angular.IScope) {
             console.log(".ctor KendoGridController", $scope);
 
             this.title = "Kendo grid";
 
             this.gridOptions = {
                 columns: [
-                    { field: "firstName", title: "First Name", width: 120 },
-                    { field: "lastName", title: "Last Name", template: `<a ng-click="vm.showUserDetails('#: lastName #')">#: lastName #</a>` },
-                    { field: "country", title: "Country", width: "120px", minScreenWidth: 900 },
+                    { field: "firstName", title: "First Name", width: "150px" },
+                    { field: "lastName", title: "Last Name", width: "150px", template: `<a ng-click="vm.showUserDetails('#: lastName #')">#: lastName #</a>` },
+                    { field: "country", title: "Country", minScreenWidth: 900 }, 
                     { field: "age", title: "Age", width: 90, minScreenWidth: 800 },
                     { field: "createdUtc", title: "Date created" }
                 ],
@@ -44,20 +45,21 @@ module App {
                 scrollable: {
                     virtual: true
                 },
-                change: function (e) {
-                    console.log("grid:change ", e, this);
-                    var selectedRows = this.select();
-                    var selectedDataItems = [];
-                    for (var i = 0; i < selectedRows.length; i++) {
-                        var dataItem = this.dataItem(selectedRows[i]);
-                        selectedDataItems.push(dataItem);
-                    }
-                }
+                selectable: "row"
             };
+        }
+
+        public handleGridChange(data, dataItem, columns): void {
+            console.log("Row selected", data, dataItem, columns);
+            this.selectedItem = data.firstName + " " + data.lastName;
         }
 
         public showUserDetails(e: any): void {
             this.selectedItem = e;
+        }
+
+        public refreshGrid(): void {
+            this.grid.dataSource.read();
         }
     }
 }
